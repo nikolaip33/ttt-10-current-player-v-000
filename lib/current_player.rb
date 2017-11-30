1c029dd456f96103886d36eccefbe490610bec14
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-30 17:27:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,6 +12,13 @@ def turn_count(board)
   9 - board.count(" ")
 end
 
+def turn_count(board)
+  num_turns = 0
+  board.each do |turns|
+    num_turns += 1 if turns != " ""
+  end
+end
+
 
 def current_player(board)
   if turn_count(board)%2 == 0
